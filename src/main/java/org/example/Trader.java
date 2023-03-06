91aa55d7e03b36078f738fcfc5cabf7430b47f8a
rom <alterin3980@gmail.com>
2023-03-06 21:41:07 +0900

    Initial commit

---
+++ src/main/java/org/example/Trader.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Trader {
+
+    private String name;
+    private String city;
+
+    public Trader(String n, String c) {
+        name = n;
+        city = c;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
